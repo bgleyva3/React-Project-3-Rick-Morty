99185e9d2595a558d3c5abf740ca0829f21607cc
Brandon Leyva <bgleyva3@gmail.com>
2021-05-17 18:54:35 -0500

    search-box animation

--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -3,29 +3,28 @@ import { useState } from "react"
 const SearchBox = ({ handleSearch }) => {
 
     const [queryInput, setQueryInput] = useState("")
-    const [showForm, setShowForm] = useState(false)
+    const [showForm, setShowForm] = useState("search-box out-noAnim")
 
     return (
         <div>
-            {
-                showForm ?
-                    <div className="search-box">
-                        <button onClick={() => setShowForm(false)}>X</button>
 
-                        <form onSubmit={(e) => {
-                            e.preventDefault()
-                            handleSearch(queryInput)
-                        }}>
-                            <p>Search Location</p>
-                            <input placeholder="Enter location" onChange={(elem) => setQueryInput(elem.target.value)}></input>
-                            <button type="submit" value="Search">Search</button>
-                        </form>
-                    </div>
-                    :
-                    <div className="open-search-button">
-                        <button onClick={() => setShowForm(true)}><i class="fas fa-search fa-lg"></i></button>
-                    </div>
-            }
+            <div className={showForm}>
+                <button onClick={() => setShowForm("search-box out")}>X</button>
+
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    handleSearch(queryInput)
+                }}>
+                    <p>Search Location</p>
+                    <input placeholder="Enter location" onChange={(elem) => setQueryInput(elem.target.value)}></input>
+                    <button type="submit" value="Search">Search</button>
+                </form>
+            </div>
+
+            <div className="open-search-button">
+                <button onClick={() => setShowForm("search-box in")}><i class="fas fa-search fa-lg"></i></button>
+            </div>
+
         </div>
     )
 }
